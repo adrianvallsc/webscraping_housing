661419261be0c41b768404e7ed1b2b7ea95ee1a1
adrianvallsc <adrianvallsc@gmail.com>
2022-11-22 18:50:55 +0100

    Final commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     parser.add_argument("pages", help="Select the number of pages to parse")
     args = parser.parse_args()
 
-    #main_idealista(args.city, default["speed"], int(args.pages))
+    main_idealista(args.city, default["speed"], int(args.pages))
     prueba = FotocasaSelenium(n_pages=int(args.pages), url=web_fotocasa,
                               city=args.city,
                               chrome_options=chrome_options)
